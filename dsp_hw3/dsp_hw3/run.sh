00f2e68ec1ea7c314257272b46d04f4be4552aad
hhccode <ms718293@gmail.com>
2018-12-06 15:39:42 +0800

    dsp hw3

--- dsp_hw3/dsp_hw3/run.sh
+++ dsp_hw3/dsp_hw3/run.sh
@@ -1,14 +1,11 @@
 
-./ngram-count -text corpus_seg.txt -write lm.cnt -order 2
-./ngram-count -read lm.cnt -lm bigram.lm -unk -order 2
 ./disambig -text testdata/1.txt -map ZhuYin-Big5.map -lm bigram.lm -order 2 > result1/1.txt
 ./disambig -text testdata/2.txt -map ZhuYin-Big5.map -lm bigram.lm -order 2 > result1/2.txt
 ./disambig -text testdata/3.txt -map ZhuYin-Big5.map -lm bigram.lm -order 2 > result1/3.txt
 ./disambig -text testdata/4.txt -map ZhuYin-Big5.map -lm bigram.lm -order 2 > result1/4.txt
-./disambig -text testdata/5.txt -map ZhuYin-Big5.map -lm bigram.lm -order 2 > result1/5.txt
+./disambig -text testdata/4.txt -map ZhuYin-Big5.map -lm bigram.lm -order 2 > result1/4.txt
 ./disambig -text testdata/6.txt -map ZhuYin-Big5.map -lm bigram.lm -order 2 > result1/6.txt
 ./disambig -text testdata/7.txt -map ZhuYin-Big5.map -lm bigram.lm -order 2 > result1/7.txt
 ./disambig -text testdata/8.txt -map ZhuYin-Big5.map -lm bigram.lm -order 2 > result1/8.txt
 ./disambig -text testdata/9.txt -map ZhuYin-Big5.map -lm bigram.lm -order 2 > result1/9.txt
 ./disambig -text testdata/10.txt -map ZhuYin-Big5.map -lm bigram.lm -order 2 > result1/10.txt
-
